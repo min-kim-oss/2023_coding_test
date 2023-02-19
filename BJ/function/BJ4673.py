0720f26e2f0ed3261adfa1168339e64a68b610a4
김민 <jangofett@naver.com>
2023-02-19 14:31:57 +0900

    FIX : more solution

--- BJ/function/BJ4673.py
+++ BJ/function/BJ4673.py
@@ -3,10 +3,23 @@ d = [0 for i in range(0,10001)]
 for i in range(1, 10001):
     num = list(map(int,str(i)))
     sum = i
-    for j in range(len(num)):
-        sum += num[j]        
+
+    #---logical solution---
+    m = i
+    while(True):
+        if m ==0 :
+            break
+        sum += (m%10)
+        m /= 10
+        m = int(m)
     if(sum <= 10000):
         d[sum] +=1
+
+    #--- python function ---
+    # for j in range(len(num)):
+    #     sum += num[j]        
+    # if(sum <= 10000):
+    #     d[sum] +=1
     
 for i in range(1,10001):
     if(d[i] == 0):
