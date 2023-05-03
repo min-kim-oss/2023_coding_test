34bb6de5b141a395ae7d3832d1e17de3f8819ecd
kimmin <jangofett@naver.com>
2023-05-03 11:30:09 +0900

    ADD : BJ1789.py

---
+++ BJ/binary-search/BJ1789.py
@@ -0,0 +1,15 @@
+n = int(input())
+card_list = list(map (int,(input().split())))
+m = int(input())
+check_list = list(map (int,(input().split())))
+
+dic = {}
+
+for i in range (n):
+    dic[card_list[i]] = 0
+
+for i in range(m):
+    if check_list[i] in dic.keys():
+        print(1, end = " ")
+    else:
+        print(0, end=" ")
